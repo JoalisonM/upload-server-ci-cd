4da564bea90edbe04acfcafd6affd557043e7b04
Joalison Ferreira <joalison.ferreira@nefex.io>
2025-03-20 21:08:56 -0300

    fix: fix health check http method to get

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
-  app.post("/health", async (request, reply) => {
+  app.get("/health", async (request, reply) => {
     await reply.status(200).send({ message: "IT'S OK!" });
   });
 }
