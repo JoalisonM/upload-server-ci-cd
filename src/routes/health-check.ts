592d5e884bf89864be4a59d16dfc49c3575fedba
Joalison Ferreira <joalison.ferreira@nefex.io>
2025-03-20 21:03:29 -0300

    new: configure role arn

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.post("/health", async (request, reply) => {
-    await reply.status(200).send({ message: "OK!" });
+    await reply.status(200).send({ message: "IT'S OK!" });
   });
 }
